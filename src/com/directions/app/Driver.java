4539e263834663fd93d73e0ee5e63836fa1958a9
PranavAngara19 <pranavangara19@ubuntu.(none)>
2014-01-10 14:59:19 -0800

    Document code & modify methods

--- src/com/directions/app/Driver.java
+++ src/com/directions/app/Driver.java
@@ -1,6 +1,7 @@
 package com.directions.app;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
@@ -8,6 +9,12 @@ import org.apache.http.HttpResponse;
 
 import com.directions.http.HttpManager;
 
+/**
+ * This class demonstrates how to use this mini application to fetch driving directions
+ * using the Google Maps Directions API.
+ * @author Pranav Angara
+ *
+ */
 public class Driver {
 
 	private final static String START_ADDRESS = "340 University Ave, Palo Alto, CA";
@@ -16,12 +23,15 @@ public class Driver {
 	public static void main(String[] args) throws Exception {
 		
 		HttpManager manager = new HttpManager();
-		// Create an array of addresses
+		
+		// Create an array of addresses & feed them to the manager
 		String [] addresses = new String[]{START_ADDRESS, END_ADDRESS};
 		manager.setAddresses(addresses);
 		
-		HttpResponse response = manager.fetchDirections();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		// Fetch the contents (JSON) of the response
+		InputStream directionStream= manager.fetchDirections();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(directionStream));
 		String line;
 		
 		while((line = reader.readLine()) != null){
--- src/com/directions/http/HttpManager.java
+++ src/com/directions/http/HttpManager.java
@@ -1,50 +1,86 @@
 package com.directions.http;
 
+import java.io.InputStream;
 import java.net.URISyntaxException;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+/**
+ * This class represents the HttpManager. The primary role of thsi class is to
+ * handle communication with the Google Maps Directions web services.
+ * @author Pranav Angara
+ *
+ */
 public final class HttpManager {
 	
 	private URIBuilder uriBuilder;
-	private	CloseableHttpClient httpClient;
+	private CloseableHttpClient httpClient;
 	private final String MAPS_URI = "http://maps.googleapis.com/maps/api/directions/json?origin=&destination=&sensor=false";
 	private String completeURI;
 	
+	/**
+	 * Constructor that instantiates a URI Builder & a HTTP client
+	 * @throws URISyntaxException
+	 */
 	public HttpManager() throws URISyntaxException{
 		this.uriBuilder = new URIBuilder(this.MAPS_URI);
 		this.completeURI = null;
 		this.httpClient = HttpClients.createDefault();
 	}
 	
+	/**
+	 * Given an array of addresses, this method adds the addresses to the MAPS_URI string.
+	 * @param addresses - The array that contains the start & end addresses
+	 * @throws URISyntaxException
+	 */
 	public void setAddresses(String [] addresses) throws URISyntaxException{
 		//Assuming first element represents start address
 		this.uriBuilder = this.uriBuilder.setParameter("origin", addresses[0]).setParameter("destination", addresses[1]);
 		this.setCompleteURI();
 	}
 	
+	/**
+	 * Sets the value of completeURI such that it contains the embedded addresses.
+	 * @throws URISyntaxException
+	 */
 	private void setCompleteURI() throws URISyntaxException{
 		this.completeURI = this.uriBuilder.build().toString();
 	}
 	
-	public String getCompleteURI() throws URISyntaxException{
+	/**
+	 * Returns completeURI if it is not null.
+	 * @return a String representing the request URI with the addresses embedded in.
+	 * @throws Exception 
+	 */
+	public String getCompleteURI() throws Exception{
 		if(this.completeURI == null){
-			return "The URI is incomplete, enter a pair of addresses first";
+			throw new Exception("The URI is incomplete, enter a pair of addresses first");
 		}
 		return this.completeURI;
 	}
 	
-	public HttpResponse fetchDirections() throws Exception{
+	/**
+	 * Returns an InputStream that contains the contents of the HttpResponse
+	 * @return an InputStream object
+	 * @throws Exception
+	 */
+	public InputStream fetchDirections() throws Exception{
 		if(this.completeURI == null){
 			throw new Exception("Cannot fetch directions. Please enter a pair of addresses first");
 		}
 		
 		final HttpGet httpGet = new HttpGet(this.completeURI);
 		final HttpResponse httpResponse = this.httpClient.execute(httpGet);
+		final int httpStatusCode = httpResponse.getStatusLine().getStatusCode();
 		
-		return httpResponse;
+		if(httpStatusCode != 200){
+			throw new Exception("Something went wrong! The HTTP status code is: " + httpStatusCode);
+		}
+		
+		return httpResponse.getEntity().getContent();
 	}
 }
